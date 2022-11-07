2bef2e20030c552436e88b68730014ad8673cfec
etmrmia <emt101@pitt.edu>
2022-11-07 17:13:20 -0500

    made router and began HomeView

--- interface_design_map_project/lib/viewmodels/home_viewmodel.dart
+++ interface_design_map_project/lib/viewmodels/home_viewmodel.dart
@@ -7,16 +7,6 @@ import 'package:provider/provider.dart';
 import '../router.dart' as LocalRouter;
 import '../constants.dart';
 
-class HomeViewModel extends StatefulWidget {
-  const HomeViewModel({super.key});
-
-  @override
-  State<HomeViewModel> createState() => _HomeViewModelState();
-}
-
-class _HomeViewModelState extends State<HomeViewModel> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
+class HomeViewModel extends ChangeNotifier {
+  List<String> buses = <String>[];
 }
