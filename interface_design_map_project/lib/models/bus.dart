c5d62a0fa60d509866735f0af517cd0375ec95e3
Janet <jam580@pitt.edu>
2022-11-15 23:57:16 -0500

    working on integrating route to bus

--- interface_design_map_project/lib/models/bus.dart
+++ interface_design_map_project/lib/models/bus.dart
@@ -1,6 +1,8 @@
+import 'package:interface_design_map_project/models/routes.dart';
 class Bus {
   final String title, alert;
+  Routes route;
+ 
 
-
-  Bus({required this.title, required this.alert});
+  Bus({required this.title, required this.alert, required this.route});
 }
