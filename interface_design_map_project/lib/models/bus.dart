ea0b8659b7fc11c26b3d11c41027195127d5d500
Janet <jam580@pitt.edu>
2022-12-06 20:57:14 -0500

    added walking directions and other functionality to fit personas

--- interface_design_map_project/lib/models/bus.dart
+++ interface_design_map_project/lib/models/bus.dart
@@ -1,9 +1,10 @@
 import 'package:interface_design_map_project/models/routes.dart';
 class Bus extends Routes {
-  final String title, alert;
+  final String title, alert, arrivalTime;
+  String stopChange="";
  // Routes route;
- 
+ List<String> directions=[]; //walking directions to bus stop
 
-  Bus({required this.title, required this.alert, required source, required destination, currentStop, required busImage});
+  Bus({required this.title, required this.alert, required this.arrivalTime, required source, required destination, currentStop, required busImage, directions, stopChange});
   
 }
